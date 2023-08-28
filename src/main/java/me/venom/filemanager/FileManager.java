e756523d5d302fa1fce2be126c16e19c0296e7c1
VenomTS <thefrostlord10@gmail.com>
2023-08-28 03:57:39 +0200

    Initial Commit

---
+++ src/main/java/me/venom/filemanager/FileManager.java
@@ -0,0 +1,86 @@
+package me.venom.filemanager;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.io.IOException;
+
+public final class FileManager
+{
+
+    private final Plugin main;
+    private final String fileName;
+
+    private File customConfigFile;
+    private FileConfiguration customConfig;
+
+    public FileManager(Plugin plugin, String fileName)
+    {
+        main = plugin;
+        this.fileName = fileName;
+    }
+
+    public FileConfiguration getFile() { return customConfig; }
+
+    public void getResourceFile()
+    {
+        customConfigFile = new File(main.getDataFolder(), fileName);
+        if(!customConfigFile.exists())
+        {
+            boolean success = customConfigFile.getParentFile().mkdirs();
+            if(success) main.saveResource(fileName, false);
+        }
+        loadFile();
+    }
+
+    public void createConfigFile()
+    {
+        customConfigFile = new File(main.getDataFolder(), fileName + ".yml");
+        if(customConfigFile.exists()) { loadFile(); }
+        else
+        {
+            try
+            {
+                if(customConfigFile.getParentFile().mkdirs() && customConfigFile.createNewFile())
+                {
+                    loadFile();
+                }
+            }
+            catch(IOException e)
+            {
+                sendErrorMessage("File " + fileName + " could not be created!");
+            }
+        }
+    }
+
+    private void loadFile()
+    {
+        customConfig = new YamlConfiguration();
+        try { customConfig.load(customConfigFile); }
+        catch(IOException | InvalidConfigurationException e)
+        {
+            sendErrorMessage("File " + fileName + " could not be loaded!");
+        }
+    }
+
+    public void saveFile()
+    {
+        try { customConfig.save(customConfigFile); }
+        catch(IOException e)
+        {
+            sendErrorMessage("File " + fileName + " could not be saved!");
+        }
+    }
+
+    public void sendErrorMessage(String... msgs)
+    {
+        for(String message : msgs)
+        {
+            main.getServer().getConsoleSender().sendMessage(message);
+        }
+    }
+
+}
